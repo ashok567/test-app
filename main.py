3e5c0697056b9403550893b139d29a68f76bb7f0
Ashok Thakur <iashok180@gmail.com>
2019-10-06 19:08:23 +0530

    handlers file added

---
+++ main.py
@@ -0,0 +1,47 @@
+from tornado.web import RequestHandler, Application
+import tornado.ioloop
+import os
+import process
+import json
+
+
+class MainHandler(RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+
+class DataHandler(RequestHandler):
+    def get(self):
+        res = process.get_data()
+        self.write({'response': json.loads(res)})
+
+
+class UserHandler(RequestHandler):
+    def get(self):
+        res = process.get_user()
+        self.write({'response': json.loads(res)})
+
+
+settings = dict(
+    template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+    # static_path=os.path.join(os.path.dirname(__file__), 'static'),
+    debug=True
+)
+
+
+def make_app():
+    return Application(
+        [
+            (r'/', MainHandler),
+            (r'/data', DataHandler),
+            (r'/user', UserHandler),
+            (r'/(.*)', tornado.web.StaticFileHandler,
+             {"path": ""})], **settings)
+
+
+port = 9000
+if __name__ == '__main__':
+    print("Server is running at "+str(port))
+    app = make_app()
+    app.listen(port)
+    tornado.ioloop.IOLoop.current().start()
