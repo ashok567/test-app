43136ba90ae0ede542331815800803b3392c9b27
Ashok Thakur <iashok180@gmail.com>
2019-10-26 23:58:30 +0530

    loader removed

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
 /* eslint-disable space-before-blocks */
-$(window).on('load', function () { $('.loader-wrapper').fadeOut('slow') })
-
 $('body').tooltip({ selector: '[title],[data-title],[data-original-title]', container: 'body', html: true, animated: 'fade' })
 
 var margin = { top: 20, right: 160, bottom: 35, left: 30 }
@@ -14,8 +12,7 @@ $(document).ready(function (){
     url: '/data'
   })
     .done(function (data){
-      console.log(width)
-      console.log(height)
+      console.log(data)
     })
     .fail(function (error) {
       alert(error)
